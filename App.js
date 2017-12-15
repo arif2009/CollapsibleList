2fd14c896691faa2e34d84ef2c00a3f534b010a2
Arif <arif.rahman2009@gmail.com>
2017-12-15 10:28:15 +0600

    088 A Quick Review and Breather

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 import { Header } from './src/components/common';
+import LibraryList from './src/components/LibraryList';
 
 export default class App extends Component<{}> {
   render() {
@@ -17,6 +12,7 @@ export default class App extends Component<{}> {
       <Provider store={createStore(reducers)}>
         <View>
           <Header headerText="Collapsable List" />
+          <LibraryList />
         </View>
       </Provider>
     );
